2863019757f29fa492dba2eb54c09ed13895b461
DESKTOP-K4JT48O\stan <josestalyn@hotmail.com>
2022-11-14 20:19:39 -0500

    carretera

--- src/Components/CarreteraImage.jsx
+++ src/Components/CarreteraImage.jsx
@@ -1,7 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
+
 const CarreteraImage = () => {
+  const canvasRef = useRef(null)
+  const disenoCarretera=()=>{
+    const canvas = canvasRef.current
+    canvas.height=250
+    canvas.width=700
+    const lineasBlancas = canvas.getContext('2d')
+    const lineaAmarilla = canvas.getContext('2d')
+    //Our first draw
+    lineasBlancas.fillStyle = '#FFFFFF'
+    lineasBlancas.fillRect(-10, 10, 100, 10)
+    lineasBlancas.fillRect(170, 10, 130, 10)
+    lineasBlancas.fillRect(380, 10, 130, 10)
+    lineasBlancas.fillRect(580, 10, 130, 10)
+    lineaAmarilla.fillStyle = '#FFC000'
+    lineaAmarilla.fillRect(-10,240,720, 10)
+  }
+  useEffect(() => {
+    disenoCarretera()
+  }, [])
   return (
-    <div style={{marginTop:'20px'}}>
+    <div style={{marginTop:'20px'}} className="flex justify-center">
+      <canvas style={{backgroundColor:'#808080'}} ref={canvasRef} >
+
+      </canvas>
     </div>
         
   )
